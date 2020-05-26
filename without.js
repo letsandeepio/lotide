6cc72240e43299e01a0abb383cab5840141a840d
Sandeep Chopra <sandeep.chopra7@gmail.com>
2020-05-26 20:52:07 +0000

    added without.js

--- without.js
+++ without.js
@@ -15,3 +15,12 @@ const assertArraysEqual = function (arr1, arr2) {
     console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`);
   }
 };
+
+const without = function (source, itemsToRemove) {
+  return source.filter((item) => !itemsToRemove.includes(item));
+};
+
+const words = ['hello', 'world', 'lighthouse'];
+without(words, ['lighthouse']); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ['hello', 'world', 'lighthouse']);
