c3d98c830ea72a7a9078e4e134336d92efd24e60
Sandeep Chopra <sandeep.chopra7@gmail.com>
2020-05-27 21:30:51 +0000

    added eqObjects.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,8 +1,19 @@
+// FUNCTION IMPLEMENTATION
 const assertEqual = function (actual, expected) {
+  const FgRed = '\x1b[31m';
+  const FgGreen = '\x1b[32m';
   if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] === [${expected}]`);
+    console.log(
+      FgRed,
+      `🛑 Assertion Failed: [${actual}] === [${expected}]`,
+      '\x1b[0m'
+    );
   } else {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+    console.log(
+      FgGreen,
+      `✅ Assertion Passed: [${actual}] === [${expected}]`,
+      '\x1b[0m'
+    );
   }
 };
 
