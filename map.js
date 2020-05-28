6f94fb30e981e0bb7785fc17d7960a573f18f489
Sandeep Chopra <sandeep.chopra7@gmail.com>
2020-05-28 19:02:28 +0000

    added map.js

---
+++ map.js
@@ -0,0 +1,32 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i += 1) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  const FgRed = '\x1b[31m';
+  const FgGreen = '\x1b[32m';
+  if (!eqArrays(arr1, arr2)) {
+    console.log(FgRed, `🛑 Assertion Failed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(FgGreen, `✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  }
+};
+
+const map = function (array, callback) {
+  const results = [];
+  for (const item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const words = ['ground', 'control', 'to', 'major', 'tom'];
+
+const results1 = map(words, (word) => word[0]);
+
