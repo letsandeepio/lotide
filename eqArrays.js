7230a3a3c297d497f7df3b46164ef72e786facd6
Sandeep Chopra <sandeep.chopra7@gmail.com>
2020-06-01 17:38:23 +0000

    added tests for tail

--- eqArrays.js
+++ eqArrays.js
@@ -1,22 +1,3 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function (actual, expected) {
-  const FgRed = '\x1b[31m';
-  const FgGreen = '\x1b[32m';
-  if (actual !== expected) {
-    console.log(
-      FgRed,
-      `🛑 Assertion Failed: [${actual}] === [${expected}]`,
-      '\x1b[0m'
-    );
-  } else {
-    console.log(
-      FgGreen,
-      `✅ Assertion Passed: [${actual}] === [${expected}]`,
-      '\x1b[0m'
-    );
-  }
-};
-
 const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
   for (let i = 0; i < arr1.length; i += 1) {
@@ -30,5 +11,4 @@ const eqArrays = function (arr1, arr2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, [3, 5]], [1, 2, [3, 4]]), true);
+module.exports = eqArrays;
