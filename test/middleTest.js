82fcf9a4354ef4c84ebd2c0738434617d38a5b94
Sandeep Chopra <sandeep.chopra7@gmail.com>
2020-06-01 18:58:28 +0000

    added mocha & chai and refactored middleTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,4 @@
-const assertArraysEqual = require('../assertArraysEqual');
+/* const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 
 assertArraysEqual(middle([1]), []);
@@ -7,4 +7,13 @@ assertArraysEqual(middle([1, 2]), []);
 assertArraysEqual(middle([1, 2, 3]), [2]);
 assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); */
+
+const assert = require('chai').assert;
+const head = require('../head');
+
+describe('#head', () => {
+  it('returns 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
